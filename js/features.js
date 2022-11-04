3b9b54047b6bbc45415e7c2e1975f968c5183f2a
afaysalk <91470600+afaysalk@users.noreply.github.com>
2022-11-05 00:50:16 +0200

    Fixed features page functionality

--- js/features.js
+++ js/features.js
@@ -1,4 +1,4 @@
-function featuresSelect(sectionID, targetID) {
+function featuresSelect(sectionID, targetID,thumbID,scrollID,bgID,imgID) {
   var resetElm = document
     .getElementById(sectionID)
     .getElementsByClassName("active");
@@ -9,4 +9,36 @@ function featuresSelect(sectionID, targetID) {
 
   var targetElm = document.getElementById(targetID);
   targetElm.classList.add("active");
+
+  var resetThumb =document
+    .getElementById(scrollID)
+    .getElementsByClassName("blue");
+    for (var i = 0; i < resetThumb.length; i++) {
+      resetThumb[i].classList.remove("blue");
+    }
+  
+    var targetElm = document.getElementById(thumbID);
+    targetElm.classList.add("blue"); 
+    
+
+    var resetImg =document
+    .getElementById(bgID)
+    .getElementsByClassName("screenshot");
+    for (var i = 0; i < resetImg.length; i++) {
+      resetImg[i].classList.remove("screenshot");
+    }
+  
+    var targetElm = document.getElementById(imgID);
+    targetElm.classList.add("screenshot"); 
+      
+}
+
+
+function mobileScroll(id) {
+  
+  document.getElementById(id).scrollIntoView({block: "nearest"});
+  
+
+
+
 }
