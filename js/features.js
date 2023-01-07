8516db4c54699c19178ba24811f01084246f816f
hummingly <31522351+hummingly@users.noreply.github.com>
2023-01-07 14:55:10 +0100

    Fix reading incorrect height

--- js/features.js
+++ js/features.js
@@ -1,18 +1,6 @@
 "use strict";
 
-// {
-//   const mediaQuery = window.matchMedia("only screen and (min-width: 36rem)");
-
-//   if (mediaQuery.matches) {
-//     requestAnimationFrame(initAccordions);
-//     mediaQuery.addEventListener("change", initTabs, { once: true });
-//   } else {
-//     requestAnimationFrame(initTabs);
-//     mediaQuery.addEventListener("change", initAccordions, { once: true });
-//   }
-// }
-
-requestAnimationFrame(initAccordions);
+initAccordions();
 
 function initAccordions() {
   const ARIA_EXPANDED = "aria-expanded";
@@ -75,9 +63,9 @@ function initAccordions() {
       const container = header.nextElementSibling;
 
       if (expandedHeader === headerIndex) {
-        scrollThumb.style.height = `${header.scrollHeight + container.scrollHeight}px`;
         scrollThumb.style.transform = "translateY(0px)";
         container.style.opacity = "1";
+        scrollThumb.style.height = `${header.scrollHeight + container.scrollHeight}px`;
       } else {
         button.setAttribute(ARIA_EXPANDED, FALSE);
         container.style.opacity = "0";
@@ -101,69 +89,5 @@ function initAccordions() {
         { capture: true }
       );
     }
-  }
-}
-
-function initTabs() {
-  const ARIA_SELECTED = "aria-selected";
-  const TRUE = "true";
-  const FALSE = "false";
-  const REGISTER_FOCUS = 0;
-  const UNREGISTER_FOCUS = -1;
-
-  for (const tabs of document.getElementsByClassName("tabs")) {
-    const tablist = tabs.firstElementChild;
-    const buttons = tablist.children;
-    const scrollThumb = tablist.nextElementSibling.firstElementChild;
-    const tabpanels = tabs.lastElementChild.children;
-    const tabCount = Math.min(buttons.length, tabpanels.length);
-    let selectedTab = 0;
-
-    tablist.addEventListener("keydown", function handleArrowKeys(event) {
-      let nextButton;
-      if (event.key === "ArrowRight") {
-        nextButton = event.target.nextElementSibling || this.firstElementChild;
-      } else if (event.key === "ArrowLeft") {
-        nextButton = event.target.previousElementSibling || this.lastElementChild;
-      } else {
-        return;
-      }
-      nextButton.focus({ preventScroll: true });
-      nextButton.click();
-    }, { capture: true });
-
-    for (let tabIndex = 0; tabIndex < tabCount; tabIndex++) {
-      buttons[tabIndex].addEventListener("click", () => selectTab(tabIndex), { capture: true });
-
-      if (selectedTab === tabIndex) {
-        buttons[tabIndex].setAttribute(ARIA_SELECTED, TRUE);
-        tabpanels[tabIndex].tabIndex = REGISTER_FOCUS;
-      } else {
-        buttons[tabIndex].tabIndex = UNREGISTER_FOCUS;
-        tabpanels[tabIndex].tabIndex = UNREGISTER_FOCUS;
-      }
-    }
-
-    const selectTab = (tabIndex) => {
-      if (selectedTab === tabIndex) {
-        return;
-      }
-
-      // Track selected tab
-      const previousIndex = selectedTab;
-      const nextIndex = tabIndex;
-      selectedTab = tabIndex;
-
-      buttons[previousIndex].setAttribute(ARIA_SELECTED, FALSE);
-      buttons[previousIndex].tabIndex = UNREGISTER_FOCUS;
-      tabpanels[previousIndex].tabIndex = UNREGISTER_FOCUS;
-
-      buttons[nextIndex].setAttribute(ARIA_SELECTED, TRUE);
-      buttons[nextIndex].tabIndex = REGISTER_FOCUS;
-      tabpanels[nextIndex].tabIndex = REGISTER_FOCUS;
-
-      scrollThumb.style.transform = `translateX(${100 * tabIndex}%)`;
-      tabpanels[nextIndex].scrollIntoView({ block: "nearest" });
-    };
   }
 }
